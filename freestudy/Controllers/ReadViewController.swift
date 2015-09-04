ac816dcf6243390fc5d3100b6517e6796c90ca7b
acuros <acuros@kaist.ac.kr>
2015-09-04 10:57:47 +0900

    Layouts

--- freestudy/Controllers/ReadViewController.swift
+++ freestudy/Controllers/ReadViewController.swift
@@ -6,9 +6,10 @@ class ReadViewController : UIViewController, UIWebViewDelegate, UIScrollViewDele
     
     var study: JSON?
     lazy var scrollView = UIScrollView()
+    
+    lazy var tagNamesView = UILabel()
     lazy var titleView = UILabel()
     lazy var contentView = UIWebView()
-    lazy var button = UIButton()
     
     init(studyId: Int) {
         super.init(nibName: nil, bundle: nil)
@@ -19,12 +20,21 @@ class ReadViewController : UIViewController, UIWebViewDelegate, UIScrollViewDele
             )
             .responseJSON { _, _, data, _ in
                 self.study = JSON(data!)["study"]
-                let titleString = self.study!["title"].stringValue
-                self.titleView.text = titleString
-                self.titleView.frame = CGRectMake(20, 20, self.view.frame.width - 40, CGFloat.max)
+                
+                let areaName = TagsManager.sharedInstance.nameOf(self.study!["area"].stringValue)
+                let categoryName = TagsManager.sharedInstance.nameOf(self.study!["category"].stringValue)
+                let tagNamesAttributedText = NSMutableAttributedString(string: "\(areaName) / \(categoryName)")
+                tagNamesAttributedText.addAttribute(NSForegroundColorAttributeName, value: UIColor(hex: "#f48210"), range: NSMakeRange(0, count(areaName)))
+                tagNamesAttributedText.addAttribute(NSForegroundColorAttributeName, value: UIColor(hex: "#a0a0a0"), range: NSMakeRange(count(areaName) + 3, count(categoryName)))
+                self.tagNamesView.attributedText = tagNamesAttributedText
+                
+                self.titleView.text = self.study!["title"].stringValue
+                self.titleView.frame.size.width = self.scrollView.frame.width - 40.0
                 self.titleView.sizeToFit()
+                self.titleView.frame.size.width = self.scrollView.frame.width - 40.0
                 
                 self.contentView.loadHTMLString(self.study!["content"].stringValue, baseURL: nil)
+                self.contentView.frame.origin = CGPointMake(0, self.titleView.frame.origin.y + self.titleView.frame.size.height + 40.0)
             }
     }
 
@@ -45,20 +55,28 @@ class ReadViewController : UIViewController, UIWebViewDelegate, UIScrollViewDele
         self.view.addSubview(scrollView)
         self.scrollView.backgroundColor = UIColor.whiteColor()
         self.scrollView.scrollEnabled = true
-        scrollView.snp_makeConstraints { (make) -> Void in
+        self.scrollView.snp_makeConstraints { (make) -> Void in
             make.edges.equalTo(self.view)
         }
     }
     
     func initTitleViewLayout() {
         self.scrollView.addSubview(titleView)
-        titleView.lineBreakMode = .ByWordWrapping
-        titleView.numberOfLines = 0
+        self.scrollView.addSubview(tagNamesView)
+        
+        self.tagNamesView.frame = CGRectMake(20.0, 20.0, self.view.frame.width - 40.0, 20.0)
+        self.tagNamesView.textAlignment = .Center
+        
+        self.titleView.frame.origin = CGPointMake(20.0, 50.0)
+        self.titleView.font = UIFont.systemFontOfSize(17.0, weight: 3.0)
+        self.titleView.textAlignment = .Center
+        self.titleView.lineBreakMode = .ByWordWrapping
+        self.titleView.numberOfLines = 0
     }
     
     func initContentViewLayout() {
         self.scrollView.addSubview(contentView)
-        self.contentView.backgroundColor = UIColor.yellowColor()
+        self.contentView.backgroundColor = UIColor.clearColor()
         self.contentView.delegate = self
         self.contentView.userInteractionEnabled = false
         self.contentView.scrollView.scrollEnabled = false
@@ -67,8 +85,10 @@ class ReadViewController : UIViewController, UIWebViewDelegate, UIScrollViewDele
     func webViewDidFinishLoad(webView: UIWebView) {
         var height = CGFloat(webView.stringByEvaluatingJavaScriptFromString("document.body.offsetHeight;")!.toInt()!)
         let newSize = CGSizeMake(scrollView.frame.size.width, height)
-        webView.frame.origin = CGPointMake(0, titleView.frame.size.height + 20.0)
         webView.frame.size = newSize
-        self.scrollView.contentSize = newSize
+        
+        var contentSize = webView.frame.size
+        contentSize.height += webView.frame.origin.y
+        self.scrollView.contentSize = contentSize
     }
 }
--- freestudy/TagsManager.swift
+++ freestudy/TagsManager.swift
@@ -10,10 +10,19 @@ class TagsManager {
     init() {
     }
     
+    func nameOf(tagValue: String) -> String {
+        if (self.displayNames[tagValue] != nil) {
+            return self.displayNames[tagValue]!
+        }
+        else {
+            return "기타"
+        }
+    }
+    
     func putTags(tags: JSON) {
         self.displayNames = Dictionary(map(tags["display_names"].dictionaryValue){
             (key, value) in (key, value.stringValue)
-            })
+        })
         self.filters = Dictionary(map(tags["filters"].dictionaryValue){
             (key:String, value:JSON) in
             (key, map(value.arrayValue){(tagName:JSON) -> String in tagName.stringValue})
