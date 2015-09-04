cdfbe0132a37766dcd888faa7a13fa65358bcdf1
acuros <acuros@kaist.ac.kr>
2015-09-04 09:06:06 +0900

    Show content

--- freestudy/Controllers/ReadViewController.swift
+++ freestudy/Controllers/ReadViewController.swift
@@ -2,11 +2,13 @@ import SnapKit
 import Alamofire
 import SwiftyJSON
 
-class ReadViewController : UIViewController {
+class ReadViewController : UIViewController, UIWebViewDelegate, UIScrollViewDelegate {
     
     var study: JSON?
+    lazy var scrollView = UIScrollView()
     lazy var titleView = UILabel()
     lazy var contentView = UIWebView()
+    lazy var button = UIButton()
     
     init(studyId: Int) {
         super.init(nibName: nil, bundle: nil)
@@ -17,7 +19,12 @@ class ReadViewController : UIViewController {
             )
             .responseJSON { _, _, data, _ in
                 self.study = JSON(data!)["study"]
-                self.titleView.text = self.study!["title"].stringValue
+                let titleString = self.study!["title"].stringValue
+                self.titleView.text = titleString
+                self.titleView.frame = CGRectMake(20, 20, self.view.frame.width - 40, CGFloat.max)
+                self.titleView.sizeToFit()
+                
+                self.contentView.loadHTMLString(self.study!["content"].stringValue, baseURL: nil)
             }
     }
 
@@ -27,18 +34,41 @@ class ReadViewController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = UIColor.whiteColor()
-        self.view.addSubview(self.titleView)
         
-        titleView.snp_makeConstraints { (make) -> Void in
-            var topLayoutGuide = self.topLayoutGuide as! UIView
-            make.top.equalTo(topLayoutGuide.snp_bottom).offset(10)
-            make.left.equalTo(self.view).offset(20)
-            make.right.equalTo(self.view).offset(-20)
+        initScrollView()
+        initTitleViewLayout()
+        initContentViewLayout()
+    }
+    
+    func initScrollView() {
+        self.view.addSubview(scrollView)
+        self.scrollView.backgroundColor = UIColor.whiteColor()
+        self.scrollView.scrollEnabled = true
+        scrollView.snp_makeConstraints { (make) -> Void in
+            make.edges.equalTo(self.view)
         }
+    }
+    
+    func initTitleViewLayout() {
+        self.scrollView.addSubview(titleView)
         titleView.lineBreakMode = .ByWordWrapping
         titleView.numberOfLines = 0
     }
     
+    func initContentViewLayout() {
+        self.scrollView.addSubview(contentView)
+        self.contentView.backgroundColor = UIColor.yellowColor()
+        self.contentView.delegate = self
+        self.contentView.userInteractionEnabled = false
+        self.contentView.scrollView.scrollEnabled = false
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        var height = CGFloat(webView.stringByEvaluatingJavaScriptFromString("document.body.offsetHeight;")!.toInt()!)
+        let newSize = CGSizeMake(scrollView.frame.size.width, height)
+        webView.frame.origin = CGPointMake(0, titleView.frame.size.height + 20.0)
+        webView.frame.size = newSize
+        self.scrollView.contentSize = newSize
+    }
 }
