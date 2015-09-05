078a8a490287157029ceca49dcdbc1da0ba2fe83
acuros <acuros@kaist.ac.kr>
2015-09-05 16:55:17 +0900

    UILabel.setHTML

---
+++ freestudy/+UILabel.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+extension UILabel {
+    func setHtml(html: String) {
+        var err: NSError?
+        self.attributedText = NSAttributedString(
+            data: html.dataUsingEncoding(NSUTF8StringEncoding)!,
+            options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
+            documentAttributes: nil,
+            error: &err
+        )
+        if err != nil {
+            println("Unable to parse label text: \(err)")
+        }
+    }
+}
